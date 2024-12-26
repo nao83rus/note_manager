f84fdb1601628bb7aeda89e1feb888888bb2daa5
nao83rus <aparnikov@yandex.ru>
2024-12-26 23:15:06 +0300

    2024-12-26

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Vladimir'
+title = 'Notes'
+content = 'My notes'
+status = 'Completed'
+created_date = '26-12-2024'
+issue_date = '31-12-24'
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date)
+print('Дата истечения заметки: ', issue_date)
