fbba2a317dcb05d7e028d2c3293f9561b67b4e4d
nao83rus <aparnikov@yandex.ru>
2024-12-30 17:55:48 +0300

    2024-12-30 date_changer.py type data

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,11 @@
-from datetime import date
+import datetime
+
 created_date = '26-12-2024'
 issue_date = '31-12-24'
-print('Created date:', created_date[0:5])
+created_date_date = int(created_date[0:2])
+created_date_month = int(created_date[3:5])
+created_date_year = int(created_date[6:])
+temp_created_date = datetime.date(created_date_year, created_date_month, created_date_date)
+#print('Created date:', created_date[0:5])
+print('Created date:', temp_created_date)
 print('Issue date:', issue_date[0:5])
