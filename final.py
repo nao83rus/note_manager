20daeccb56dbc9913910bc3b201135dd4a8f85d4
nao83rus <aparnikov@yandex.ru>
2025-01-08 17:49:49 +0300

    2025-01-08 add_titles_loop.py

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 heading = ["username", "content", "status", "created_at", "updated_at", "titles"]
-note1 = [input("Введите Ваше имя: "), input("Введите содержание заметки: "), input("Введите статус заметки: "),
+note_data = [input("Введите Ваше имя: "), input("Введите содержание заметки: "), input("Введите статус заметки: "),
          input('Введите дату начала в формате \"дата-месяц-год\": '), input('Введите дату изменения в формате \"дата-месяц-год\": ')]
-note1_1 = [input('Введите заголовок 1: '), input('Введите заголовок 2: ')]
-note1.append(note1_1)
-note = dict(zip(heading, note1))
+note_titles = [input('Введите заголовок 1: '), input('Введите заголовок 2: ')]
+note_data.append(note_titles)
+note = dict(zip(heading, note_data))
 print(note)
