d2cca4e8c737ca405da1cbba4a5463db825a34fb
nao83rus <aparnikov@yandex.ru>
2025-01-09 23:19:29 +0300

    2025-01-09 check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -7,16 +7,17 @@ print('Текущая дата: ', today)
 while True:
     try:
         # Запрашиваем дату дедлайна у пользователя
-        deadline_str = input("Введите дату дедлайна (в формате день-месяц-год, например 11-01-2025): ")
+        issue_date = input("Введите дату дедлайна (в формате день-месяц-год, например 31-01-2025): ")
 
         # Преобразуем строку с датой в объект datetime
-        deadline_date = datetime.strptime(deadline_str, "%d-%m-%Y")
-        # Вычисляем разницу между текущей датой и дедлайном
+        deadline_date = datetime.strptime(issue_date, "%d-%m-%Y")
+        # Вычисляем разницу между текущей датой и дедлайном. Учитываем только дату без времени.
         time_difference = deadline_date.date() - today.date()
         days_difference = time_difference.days
         # Проверяем статус дедлайна и выводим соответствующее сообщение
         if days_difference < 0:
-            print(f"Внимание! Дедлайн истёк {days_difference} д. назад.")
+            # abs() убирает минус в строке вывода и выводит абсолютное значение
+            print(f"Внимание! Дедлайн истёк {abs(days_difference)} д. назад.")
         elif days_difference == 0:
             print("Дедлайн сегодня!")
         else:
@@ -28,7 +29,7 @@ while True:
     except ValueError:
         # Обработка ошибки неверного формата даты
         print("Ошибка! Пожалуйста, введите дату в правильном формате (день-месяц-год).")
-        print("Пример: 11-01-2025")
+        print("Пример: 31-01-2025")
 
     except Exception as e:
         # Обработка прочих ошибок
