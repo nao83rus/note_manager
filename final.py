ef143479d000aaa7d372d82385fda113fda468c3
nao83rus <aparnikov@yandex.ru>
2025-01-10 20:01:43 +0300

    2025-01-10 исправил замечания в add_list.py и final.py

--- final.py
+++ final.py
@@ -1,7 +1,10 @@
 heading = ["username", "content", "status", "created_at", "updated_at", "titles"]
 note_data = [input("Введите Ваше имя: "), input("Введите содержание заметки: "), input("Введите статус заметки: "),
          input('Введите дату начала в формате \"дата-месяц-год\": '), input('Введите дату изменения в формате \"дата-месяц-год\": ')]
-note_titles = [input('Введите заголовок 1: '), input('Введите заголовок 2: ')]
+title1 = input('Введите заголовок заметки 1: ')
+title2 = input('Введите заголовок заметки 2: ')
+title3 = input('Введите заголовок заметки 3: ')
+note_titles = [title1, title2, title3]
 note_data.append(note_titles)
 note = dict(zip(heading, note_data))
 print(note)
