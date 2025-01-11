efccdc95a1fcedc7e472769520065e188e26afcf
nao83rus <aparnikov@yandex.ru>
2025-01-11 17:27:46 +0300

    2025-01-11 Grade 1. Этап 2: Задание 4 даты в нескольких форматах

--- multiple_notes.py
+++ multiple_notes.py
@@ -19,8 +19,40 @@ while True:
                 break
             else:
                 print("Введён неверный статус, попробуйте еще раз.")
-        created_date = input('Введите дату создания (день-месяц-год): ')
-        deadline_date = input('Введите дедлайн (день-месяц-год): ')
+        while True:
+            try:
+                created_tmp = input('Введите дату создания (ДД-ММ-ГГГГ или ГГГГ-ММ-ДД): ')
+                if created_tmp[5] == '-':
+                    if datetime.strptime(created_tmp, "%d-%m-%Y"):
+                        created_date = datetime.strptime(created_tmp, "%d-%m-%Y")
+                        break
+                if created_tmp[7] == '-':
+                    if datetime.strptime(created_tmp, "%Y-%m-%d"):
+                        created_date = datetime.strptime(created_tmp, "%Y-%m-%d")
+                        break
+            except ValueError:
+                print("Введена некорректная дата, попробуйте ещё раз.")
+            except Exception as e:
+                # Обработка прочих ошибок
+                print(f"Произошла непредвиденная ошибка: {str(e)}")
+                print("Пожалуйста, попробуйте снова.")
+        while True:
+            try:
+                deadline_tmp = input('Введите дату создания (ДД-ММ-ГГГГ или ГГГГ-ММ-ДД): ')
+                if deadline_tmp[5] == '-':
+                    if datetime.strptime(deadline_tmp, "%d-%m-%Y"):
+                        deadline_date = datetime.strptime(deadline_tmp, "%d-%m-%Y")
+                        break
+                if created_tmp[7] == '-':
+                    if datetime.strptime(deadline_tmp, "%Y-%m-%d"):
+                        deadline_date = datetime.strptime(deadline_tmp, "%Y-%m-%d")
+                        break
+            except ValueError:
+                print("Введена некорректная дата, попробуйте ещё раз.")
+            except Exception as e:
+                # Обработка прочих ошибок
+                print(f"Произошла непредвиденная ошибка: {str(e)}")
+                print("Пожалуйста, попробуйте снова.")
         note_data = [username, content, description, status, created_date, deadline_date]
         note = dict(zip(heading, note_data))
         notes.append(note)
