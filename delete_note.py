a57447dea215afa4147c21c3a61d31ab95f9d11f
nao83rus <aparnikov@yandex.ru>
2025-01-12 18:11:16 +0300

    2025-01-12 # Grade 1. Этап 2: Задание 5 Удаление заметок

---
+++ delete_note.py
@@ -0,0 +1,33 @@
+# Grade 1. Этап 2: Задание 5 Удаление заметок
+notes = []
+heading = ["name", "content", "description"]
+
+# Добавим несколько заметок
+note_data = ["Владимр", "Домашка", "Задание 5"]
+note = dict(zip(heading, note_data))
+notes.append(note)
+note_data = ["Андрей", "Магазин", "Купить молоко"]
+note = dict(zip(heading, note_data))
+notes.append(note)
+note_data = ["Александр", "Тренировка", "Бассейн"]
+note = dict(zip(heading, note_data))
+notes.append(note)
+
+# while True:
+#     answer = input("Для добавления следующей заметки введите 1 или другое для прекращения: ")
+#     if answer == "1":
+#         name = input("Введите имя: ")
+#         title = input("Введите заголовок: ")
+#         description = input("Введите описание: ")
+#         note_data = [name, title, description]
+#         notes.append(dict(zip(note_title, note_data)))
+#     else:
+#         break
+number = 0
+for note_number in notes:
+    number = number + 1
+    print(notes)
+
+# Удаление заметок
+del_answer = input("Для удаления заметки нажмите 1 или другое, чтобы выйти: ")
+if del_answer == "1":
