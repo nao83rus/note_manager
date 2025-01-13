09e00a15228d305705d58b7d9a46c4cc445df14d
nao83rus <aparnikov@yandex.ru>
2025-01-13 22:39:34 +0300

    2025-01-12 # Grade 1. Этап 2: Задание 5 Удаление заметок (моё решение)

--- delete_note.py
+++ delete_note.py
@@ -1,6 +1,6 @@
 # Grade 1. Этап 2: Задание 5 Удаление заметок
 notes = []
-heading = ["name", "content", "description"]
+heading = ["Имя", "Заголовок", "Комментарий"]
 
 # Добавим несколько заметок
 note_data = ["Владимр", "Домашка", "Задание 5"]
@@ -13,35 +13,33 @@ note_data = ["Александр", "Тренировка", "Бассейн"]
 note = dict(zip(heading, note_data))
 notes.append(note)
 
-# while True:
-#     answer = input("Для добавления следующей заметки введите 1 или другое для прекращения: ")
-#     if answer == "1":
-#         name = input("Введите имя: ")
-#         title = input("Введите заголовок: ")
-#         description = input("Введите описание: ")
-#         note_data = [name, title, description]
-#         notes.append(dict(zip(note_title, note_data)))
-#     else:
-#         break
-# number = 0
-# for note_number in notes:
-#     number = number + 1
-#     print(notes)
+# Ввыод на экран текущих заметок
+def print_notes(notes):
+    number = len(notes)
+    while number > 0:
+        print("Заметка", number)
+        number = number - 1
+        note_tmp = notes[number]
+        print(note_tmp.items())
+
+print_notes(notes)
 
 # Удаление заметок
 del_answer = input("Для удаления заметки нажмите 1 или другое, чтобы выйти: ")
 if del_answer == "1":
-    del_keys = "Владимр" #input("Введите значение для поиска и удаления: ")
-    # notes.remove(del_keys)
+    del_keys = input("Введите значение для поиска и удаления: ")
+    # Получим количество элементов списка заметок
     num = len(notes)
     while num > 0:
         num = num - 1
         try:
+            # Извлечем словари из списка
             list_tmp = notes[num]
+            # Получим значения ключей словаря и проверим на содержание искомой строки
             tmp = list_tmp.values()
             if del_keys in tmp:
+                # Нашли искомую строку в значениях ключей, удаляем словарь
                 notes.remove(list_tmp)
-            # print(notes[num])
         except KeyError:
             pass
-print(notes)
+print_notes(notes)
