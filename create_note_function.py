238ad618f8840849f755969c65a6b654f10924f4
nao83rus <aparnikov@yandex.ru>
2025-01-14 23:42:15 +0300

    2025-01-14 Grade 1. Этап 3: Задание 1

---
+++ create_note_function.py
@@ -0,0 +1,79 @@
+# create_note_function.py Grade 1. Этап 3: Задание 1 Напишите функцию для создания новой заметки и возврата словаря.
+from datetime import datetime
+
+# Создаём пустой список заметок
+notes = []
+
+# Функция добавления заметки
+def create_note():
+    heading = ["username", "title", "description", "status", "created_date", "deadline_date"]
+    username = input("ВВведите имя пользователя: ")
+    content = input("Введите заголовок заметки: ")
+    description = input("Введите описание заметки: ")
+    while True:
+        status = input("Введите статус заметки (1. новая, 2. в процессе, 3. выполнено): ")
+        status = status.lower()
+        # Проверяем корректность ввода статуса
+        if status == '1' or status.lower() == 'новая':
+            status = 'новая'
+            break
+        elif status == '2' or status.lower() == 'в процессе':
+            status = 'в процессе'
+            break
+        elif status == '3' or status.lower() == 'выполнено':
+            status = 'выполнено'
+            break
+        else:
+            print("Введён неверный статус, попробуйте еще раз.")
+    while True:
+        try:
+            created_tmp = input('Введите дату создания (ДД-ММ-ГГГГ или ГГГГ-ММ-ДД): ')
+            if created_tmp[5] == '-':
+                if datetime.strptime(created_tmp, "%d-%m-%Y"):
+                    created_date = datetime.strptime(created_tmp, "%d-%m-%Y")
+                    break
+            if created_tmp[7] == '-':
+                if datetime.strptime(created_tmp, "%Y-%m-%d"):
+                    created_date = datetime.strptime(created_tmp, "%Y-%m-%d")
+                    break
+        except ValueError:
+            print("Введена некорректная дата, попробуйте ещё раз.")
+        except Exception as e:
+            # Обработка прочих ошибок
+            print(f"Произошла непредвиденная ошибка: {str(e)}")
+            print("Пожалуйста, попробуйте снова.")
+    while True:
+        try:
+            deadline_tmp = input('Введите дату дедлайна (ДД-ММ-ГГГГ или ГГГГ-ММ-ДД): ')
+            if deadline_tmp[5] == '-':
+                if datetime.strptime(deadline_tmp, "%d-%m-%Y"):
+                    deadline_date = datetime.strptime(deadline_tmp, "%d-%m-%Y")
+                    break
+            if created_tmp[7] == '-':
+                if datetime.strptime(deadline_tmp, "%Y-%m-%d"):
+                    deadline_date = datetime.strptime(deadline_tmp, "%Y-%m-%d")
+                    break
+        except ValueError:
+            print("Введена некорректная дата, попробуйте ещё раз.")
+        except Exception as e:
+            # Обработка прочих ошибок
+            print(f"Произошла непредвиденная ошибка: {str(e)}")
+            print("Пожалуйста, попробуйте снова.")
+    note_data = [username, content, description, status, created_date.date(), deadline_date.date()]
+    note = dict(zip(heading, note_data))
+    notes.append(note)
+    return notes
+
+create_note()
+print(notes)
+# Выводим список созданных заметок на экран
+counter = 1
+for note in notes:
+    print(f"Заметка {counter}.")
+    print(f"        Имя: {note['username']}")
+    print(f"        Заголовок: {note['title']}")
+    print(f"        Описание: {note['description']}")
+    print(f"        Статус: {note['status']}")
+    print(f"        Дата создания: {note['created_date']}")
+    print(f"        Дедлайн: {note['deadline_date']}")
+    counter = counter + 1
