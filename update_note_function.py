ee14e3191d8a1921ecd2babd94f618e6aa7aad7d
nao83rus <aparnikov@yandex.ru>
2025-01-17 00:02:30 +0300

    2025-01-16 Нужно еще добавить проверку обновленных данных

--- update_note_function.py
+++ update_note_function.py
@@ -1,8 +1,6 @@
 # update_note_function.py Grade 1. Этап 3: Задание 2 Функция обновления заметки
 from datetime import date
 
-from test_file import heading
-
 # Предопределим список заметок
 notes = [
     {
@@ -28,17 +26,54 @@ notes = [
     }
 ]
 
+# Функция обновления заметки
+def update_note(editable_note):
+    # Список полей (ключей в словаре)
+    list_key = []
+    while True:
+        # Получаем поле для изменения
+        field = input(f"выберите поле для обновления: ")
+        for note in notes:
+            current_key = note.keys()
+            for key in current_key:
+                list_key.append(key)
+            break
+        # Проверяем корректность ввода поля для изменения
+        if field not in list_key:
+            # Нет такого поля (ключа) - запрашиваем еще раз
+            print("Выбрано неверное поле. Попробуйте еще раз")
+        else:
+            break
+
+    # Получим новое значение поля
+    field_value = input(f"Введите новое значение поля {field}: ")
+
+    editable_note[field] = field_value
+    return editable_note
+
 # Покажем имеющиеся заметки
+for index, note in enumerate(notes, start= 1):
+    print(f" \nЗаметка номер {index}")
+    for key, value in note.items():
+          print(f"{key}: {value}")
+
+# Получаем заметку для изменения
+while True:
+    tmp_number = int(input(f"\nКакую заметку будем менять? Введите номер: "))
+    # Проверяем корректность ввода номера заметки
+    if tmp_number >= 0 and tmp_number <= notes.__len__():
+        note_number = tmp_number - 1
+        editable_note = notes[note_number]
+        break
+    else:
+        print("Введено неверное значение. Попробуйте еще раз.")
+
+update_note(editable_note)
+print("\nОтредактированная заметка:")
+for key, value in editable_note.items():
+    print(f"{key}: {value}")
 
 for index, note in enumerate(notes, start= 1):
-     print(f" \nЗаметка номер {index}")
-     for key, value in note.items():
+    print(f" \nЗаметка номер {index}")
+    for key, value in note.items():
           print(f"{key}: {value}")
-# Получаем номер заметки для изменения
-note_number = input(f"\n Какую заметку будем менять? Введите номер: ")
-
-# Получаем поле для изменения
-field = input(f"выберите поле для обновления: ")
-# Функция обновления заметки
-def update_note():
-    pass
