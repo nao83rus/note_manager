b6ee8bd02ef2c9070bc0e9f35e69546d4dbb36ce
nao83rus <aparnikov@yandex.ru>
2025-01-18 01:21:48 +0300

    2025-01-17 Grade 1. Этап 3: Задание 3 Функция отображения заметок

---
+++ display_notes_function.py
@@ -0,0 +1,79 @@
+# display_notes_function.py Grade 1. Этап 3: Задание 3 Функция отображения заметок
+import datetime
+from datetime import date
+from colorama import init, Fore, Style
+
+# Инициализация библиотеки colorama
+init(autoreset=True)
+
+def display_notes(notes):
+    answer_sort = input("Сделать сортировку по дате дедлайна (да/нет)?")
+    if answer_sort == 'да':
+        sorted_notes = sorted(notes, key=lambda x: x['issue_date'])
+        notes = sorted_notes
+    # Выводим список заметок на экран
+    # Если список заметок пуст - выводим об этом сообщение
+    while True:
+        answer = int(input('''Как вы хотите просмотреть заметки (1/2):
+        1. Только заголовки с дедлайном
+        2. Полные заметки
+        '''))
+        if answer == 2:
+            if len(notes) == 0:
+                print("У вас нет сохранённых заметок.")
+            else:
+                print("Список заметок:")
+                counter = 1
+                for note in notes:
+                    print(f"Заметка {counter}.")
+                    print(f"{Fore.GREEN}        Имя пользователя: {note['username']}")
+                    print(f"{Fore.YELLOW}        Заголовок: {note['title']}")
+                    print(f"{Fore.MAGENTA}        Описание: {note['content']}")
+                    print(f"{Fore.BLUE}        Статус: {note['status']}")
+                    print(f"{Fore.LIGHTCYAN_EX}        Дата создания: {note['created_date']}")
+                    print(f"{Fore.RED}        Дедлайн: {note['issue_date']}")
+                    print(f"{Fore.LIGHTGREEN_EX}_______________________________________________")
+                    counter = counter + 1
+            break
+        elif answer == 1:
+            print("Список заголовков заметок с дедлайном:")
+            counter = 1
+            for note in notes:
+                print(f"Заметка {counter}.")
+                print(f"{Fore.RED}        Заголовок: {note['title']}")
+                print(f"{Fore.GREEN}        Дедлайн: {note['issue_date']}")
+                print(f"{Fore.BLUE}_______________________________________________")
+                counter = counter + 1
+            break
+        else:
+            print("Введено неверное значение. Попробуйте ещё раз")
+
+# Предопределённый список заметок
+notes = [
+        {
+            'username': 'Алексей',
+            'title': 'Список покупок',
+            'content': 'Купить продукты на неделю',
+            'status': 'новая',
+            'created_date': date(2025,1,10),
+            'issue_date': date(2025,1,30)
+        },
+        {
+            'username': 'Мария',
+            'title': 'Учеба',
+            'content': 'Подготовиться к экзамену',
+            'status': 'новая',
+            'created_date': date(2025,1,9),
+            'issue_date': date(2025,2,20)
+        },
+        {
+            'username': 'Алексей',
+            'title': 'Встреча',
+            'content': 'Встреча с друзьями',
+            'status': 'новая',
+            'created_date': date(2025,1,8),
+            'issue_date': date(2025,1,21)
+        }
+    ]
+
+display_notes(notes)
