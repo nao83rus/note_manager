890b87aa33dd49ab79c2aa16c2420f4d93e44363
nao83rus <aparnikov@yandex.ru>
2025-01-21 21:17:08 +0300

    2025-01-21 Grade 1. Этап 4: Задание 2 Загрузка заметок из файла

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,31 +1,35 @@
 # load_notes_from_file.py Grade 1. Этап 4: Задание 2 Загрузка заметок из файла
-from dataclasses import replace
-
+from colorama import init, Fore, Style
 from transliterate import translit
+init(autoreset=True)
 
 def load_notes_from_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         # Читаем файл построчно
         lines = file.readlines()
-        # инициализируем пустой словарь
-        note = {}
-        # итерируем по каждой строке и разбиваем пары ключ-значение
-        for line in lines:
-            # Проверяем на разделитель заметок, если есть - добавляем в словарь
-            if '__' in line:
-                notes.append(note)
-            # Проверяем ':' если его нет - пропускаем выполнение
-            elif ':' not in line:
-                continue
-            else:
-                # разделяем каждую строку по двоеточию
-                key, value = line.strip().split(':')
-                # Транслитирируем кириллицу в латиницу
-                key = translit(key.lower(), language_code='ru', reversed=True)
-                # В ключе вместо пробела поставим '_'
-                key = key.replace(' ', '_')
-                # добавляем в словарь
-                note[key.strip()] = value.strip()
+        # Проверяем, не пустой ли файл
+        if len(lines) > 0:
+            # инициализируем пустой словарь
+            note = {}
+            # итерируем по каждой строке и разбиваем пары ключ-значение
+            for line in lines:
+                # Проверяем на разделитель заметок, если есть - добавляем в словарь
+                if '__' in line:
+                    notes.append(note)
+                # Проверяем ':' если его нет - пропускаем выполнение
+                elif ':' not in line:
+                    continue
+                else:
+                    # разделяем каждую строку по двоеточию
+                    key, value = line.strip().split(':')
+                    # Транслитирируем кириллицу в латиницу
+                    key = translit(key.lower(), language_code='ru', reversed=True)
+                    # В ключе вместо пробела поставим '_'
+                    key = key.replace(' ', '_')
+                    # добавляем в словарь
+                    note[key.strip()] = value.strip()
+        else:
+            print(f"{Fore.RED}Файл {filename} пуст.")
 
     return notes
 
