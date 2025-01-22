25666fa6a5041112ead58dac3ac00e823678a13a
nao83rus <aparnikov@yandex.ru>
2025-01-22 20:29:33 +0300

    2025-01-22

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,33 +1,37 @@
 # append_notes_to_file.py Grade 1. Этап 4: Задание 4 Добавление данных в файл
-from transliterate import translit
 from colorama import init, Fore, Style
 init(autoreset=True)
 
 def load_notes_from_file(filename):
+    # инициализируем пустой словарь
+    note = {}
+    # инициализируем список ключей
+    note_key = []
+    # Словарь транслитерации ключей
+    translate_table = {
+        "Имя пользователя": "username",
+        "Заголовок": "title",
+        "Описание": "content",
+        "Статус": "status",
+        "Дата создания": "created_date",
+        "Дедлайн": "issue_date"
+    }
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             # Читаем файл построчно
             lines = file.readlines()
             # Проверяем, не пустой ли файл
             if len(lines) > 0:
-                # инициализируем пустой словарь
-                note = {}
-                # инициализируем список ключей
-                note_key = []
                 # итерируем по каждой строке и разбиваем пары ключ-значение
                 for line in lines:
                     # Проверяем на разделитель заметок, если есть - добавляем в словарь
                     if ':' in line:
                         # разделяем каждую строку по двоеточию
-                        key, value = line.strip().split(':')
-                        if key not in note_key:
+                        verbose_key, value = line.strip().split(':')
+                        if verbose_key not in note_key:
                             # Добавим ключ в список ключей для отслеживания повторений
-                            note_key.append(key)
-                            # Транслитирируем кириллицу в латиницу
-                            key = translit(key.lower(), language_code='ru', reversed=True)
-                            # В ключе вместо пробела поставим '_'
-                            key = key.replace(' ', '_')
-                            # добавляем в словарь
+                            note_key.append(verbose_key)
+                            key = translate_table[verbose_key]
                             note[key.strip()] = value.strip()
                         else:
                             # Такой ключ уже есть. Добавляем словарь в список и очищаем список ключей
